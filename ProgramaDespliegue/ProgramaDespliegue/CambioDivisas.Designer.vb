202d287185fc474ebbe92078953bf1a99a5e11ef
GNR092 <genercan092@gmail.com>
2022-01-17 02:52:48 -0600

    Programa depliegue

---
+++ ProgramaDespliegue/ProgramaDespliegue/CambioDivisas.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CambioDivisas
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim lb_cambio As System.Windows.Forms.Label
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CambioDivisas))
+        Me.btn_convert = New System.Windows.Forms.Button()
+        Me.lb_uni = New System.Windows.Forms.Label()
+        Me.txt_moneda = New System.Windows.Forms.TextBox()
+        Me.txt_pesoMXN = New System.Windows.Forms.TextBox()
+        Me.cmb_tipodecambio = New System.Windows.Forms.ComboBox()
+        Me.lb_mxn = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        lb_cambio = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btn_convert
+        '
+        Me.btn_convert.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_convert.Location = New System.Drawing.Point(104, 112)
+        Me.btn_convert.Name = "btn_convert"
+        Me.btn_convert.Size = New System.Drawing.Size(75, 23)
+        Me.btn_convert.TabIndex = 5
+        Me.btn_convert.Text = "Convertir"
+        Me.btn_convert.UseVisualStyleBackColor = True
+        '
+        'lb_uni
+        '
+        Me.lb_uni.BackColor = System.Drawing.SystemColors.Control
+        Me.lb_uni.Font = New System.Drawing.Font("Consolas", 25.0!)
+        Me.lb_uni.ForeColor = System.Drawing.Color.Black
+        Me.lb_uni.Location = New System.Drawing.Point(75, 51)
+        Me.lb_uni.Name = "lb_uni"
+        Me.lb_uni.Size = New System.Drawing.Size(27, 34)
+        Me.lb_uni.TabIndex = 5
+        Me.lb_uni.Text = "↓ "
+        '
+        'txt_moneda
+        '
+        Me.txt_moneda.Location = New System.Drawing.Point(43, 31)
+        Me.txt_moneda.Name = "txt_moneda"
+        Me.txt_moneda.Size = New System.Drawing.Size(100, 20)
+        Me.txt_moneda.TabIndex = 3
+        '
+        'txt_pesoMXN
+        '
+        Me.txt_pesoMXN.Location = New System.Drawing.Point(43, 88)
+        Me.txt_pesoMXN.Name = "txt_pesoMXN"
+        Me.txt_pesoMXN.ReadOnly = True
+        Me.txt_pesoMXN.Size = New System.Drawing.Size(100, 20)
+        Me.txt_pesoMXN.TabIndex = 0
+        '
+        'lb_cambio
+        '
+        lb_cambio.AutoSize = True
+        lb_cambio.BackColor = System.Drawing.Color.Transparent
+        lb_cambio.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        lb_cambio.ForeColor = System.Drawing.Color.Black
+        lb_cambio.Location = New System.Drawing.Point(146, 12)
+        lb_cambio.Name = "lb_cambio"
+        lb_cambio.Size = New System.Drawing.Size(94, 13)
+        lb_cambio.TabIndex = 2
+        lb_cambio.Text = "Tipo de cambio"
+        '
+        'cmb_tipodecambio
+        '
+        Me.cmb_tipodecambio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmb_tipodecambio.FormattingEnabled = True
+        Me.cmb_tipodecambio.Items.AddRange(New Object() {"Euro (EUR)", "Dolar (USD)", "Yen (YEN)", "Dirham (AED)", "Yuan (CNY)", "Rublo (RUB)"})
+        Me.cmb_tipodecambio.Location = New System.Drawing.Point(149, 30)
+        Me.cmb_tipodecambio.Name = "cmb_tipodecambio"
+        Me.cmb_tipodecambio.Size = New System.Drawing.Size(85, 21)
+        Me.cmb_tipodecambio.TabIndex = 4
+        '
+        'lb_mxn
+        '
+        Me.lb_mxn.AutoSize = True
+        Me.lb_mxn.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lb_mxn.Location = New System.Drawing.Point(149, 90)
+        Me.lb_mxn.Name = "lb_mxn"
+        Me.lb_mxn.Size = New System.Drawing.Size(140, 15)
+        Me.lb_mxn.TabIndex = 6
+        Me.lb_mxn.Text = "Peso Mexicano (MXN)"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Controls.Add(Me.lb_uni)
+        Me.GroupBox1.Controls.Add(Me.lb_mxn)
+        Me.GroupBox1.Controls.Add(Me.btn_convert)
+        Me.GroupBox1.Controls.Add(Me.cmb_tipodecambio)
+        Me.GroupBox1.Controls.Add(Me.txt_moneda)
+        Me.GroupBox1.Controls.Add(lb_cambio)
+        Me.GroupBox1.Controls.Add(Me.txt_pesoMXN)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(292, 212)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(317, 24)
+        Me.MenuStrip1.TabIndex = 8
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(71, 20)
+        Me.AboutToolStripMenuItem.Text = "Acerca de"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.ProgramaDespliegue.My.Resources.Resources.images
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(44, 141)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(199, 71)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'CambioDivisas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(317, 244)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.Name = "CambioDivisas"
+        Me.Text = "Cambio de divisas"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn_convert As System.Windows.Forms.Button
+    Friend WithEvents lb_uni As System.Windows.Forms.Label
+    Friend WithEvents txt_moneda As System.Windows.Forms.TextBox
+    Friend WithEvents txt_pesoMXN As System.Windows.Forms.TextBox
+    Friend WithEvents cmb_tipodecambio As System.Windows.Forms.ComboBox
+    Friend WithEvents lb_mxn As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ ProgramaDespliegue/ProgramaDespliegue/CambioDivisas.vb
@@ -0,0 +1,143 @@
+﻿Imports Newtonsoft.Json.Linq
+Imports System.IO
+Imports System.Net
+''' <summary>
+''' Cambio de divisas usando APIRest de Banxico
+''' Equipo Conformado por 
+''' Gener de Jesús Can Dzul
+''' Karla Pamela Ku Pool
+''' ISC 1-A
+''' </summary>
+Public Class CambioDivisas
+
+    'Diccionario
+    Protected Indentificador As New Dictionary(Of String, String)
+
+
+
+    Private Sub btn_convert_Click(sender As Object, e As EventArgs) Handles btn_convert.Click
+        'Se valida que en el combobox este seleccionado un tipo de moneda de lo contrario se manda un MessageBox
+        If String.IsNullOrEmpty(cmb_tipodecambio.Text) Then
+            MessageBox.Show("Seleccione una moneda a convertir", "Selecione moneda", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+        'se hace un split al texto del combobox y se ontiene el texto en minusculas
+        Dim moneda As String = LCase(Split(cmb_tipodecambio.Text, " ")(0))
+        'Se le pasa a la Clase Divisa el tipo de moneda y el diccionario
+        Dim div As New Divisa(moneda, Indentificador)
+        'Obtengo y evaluo el textbox
+        Dim txbmoneda As Double = Val(txt_moneda.Text)
+        'se hace la conversion
+        Dim convert As Double = (div.Precio * txbmoneda)
+        'y se retorna al textbox en un formato simple
+        txt_pesoMXN.Text = String.Format("{0,12: $0.00}", convert)
+
+
+    End Sub
+
+    Private Sub CambioDivisas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Agregamos al diccionaro las ids de las monedas al cargar el formulario
+        Indentificador.Add("euro", "SF57923")
+        Indentificador.Add("dolar", "SF43718")
+        Indentificador.Add("yen", "SF46406")
+        Indentificador.Add("dirham", "SF57797")
+        Indentificador.Add("yuan", "SF57775")
+        Indentificador.Add("rublo", "SF57807")
+        txt_moneda.Select()
+    End Sub
+    ''' <summary>
+    ''' Esta funcion evita que se ingresen letras
+    ''' </summary>
+    ''' <param name="e">numeros en formato de cadena</param>
+    ''' <returns>True si contiene letras y false si solo numeros</returns>
+    Private Function Numerico(e As String)
+        Const _N As String = "1234567890."
+        If Not (_N.Contains(e)) Then
+            Return True
+        End If
+        Return False
+    End Function
+
+    ''' <summary>
+    ''' Esta funcion evita que el usuario ingrese letras y solo permite que los numeros sean validos en el textbox
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub txt_moneda_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt_moneda.KeyPress, cmb_tipodecambio.KeyPress
+        'Detecta si el usuario presiona la tecla Intro y hace un tab al siguiente control
+        If e.KeyChar = ChrW(13) Then
+            e.Handled = True
+            SendKeys.Send("{TAB}")
+        End If
+        'la funcion Numerico evita que se ingresen letras y retorna false en caso de que no contenga
+        'y permite que se ingrese los valores
+        e.Handled = Numerico(e.KeyChar)
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("Creado por los integrantes:" + Environment.NewLine +
+                        "Can Dzul Gener de Jesús" + Environment.NewLine +
+                        "Ku Pool Karla Pamela" + Environment.NewLine +
+                        "usando la API de https://www.banxico.org.mx/SieAPIRest/service/v1/",
+                        "Acerca de", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    End Sub
+End Class
+
+Public Class Divisa
+    Public Id As String
+    Public Precio As Double
+    ''' <summary>
+    ''' Clase auxiliar en el codigo encargada de usar el APIRest de Banxico
+    ''' </summary>
+    ''' <param name="m">Tipo de moneda</param>
+    ''' <param name="dic">Diccionario</param>
+    Public Sub New(m As String, dic As Dictionary(Of String, String))
+        'Se obtiene el id del diccionario
+        Id = dic(m)
+        'se le agrega al id a la url de la api
+        Dim Url As String = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/" + Id + "/datos/oportuno?token=dad39cd11c4aec3ba84c222fdb767e2d6df55b341d5c41a50c4c331431e6007f"
+        'se hace la consulta y retorna el valor
+        Precio = Val(GetJson(Url))
+
+    End Sub
+    ''' <summary>
+    ''' Se encarga de hacer la consulta a la api donde retorna en formato JSON
+    ''' </summary>
+    ''' <param name="url">Direccion de la API</param>
+    ''' <returns>En formato String el valor de la moneda</returns>
+    Public Function GetJson(url As String)
+        Try
+            'se encarga de usar el protocolo TSL version 1 y 2
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+            'se hacer la peticion
+            Dim request As HttpWebRequest = WebRequest.Create(url)
+            'request.ServicePoint.Expect100Continue = False 'Auxiliar en caso de error POST
+            'se obtiene la respuesta de parte de banxico
+            Using response As HttpWebResponse = request.GetResponse()
+                'se compara en caso de no dar una respuesta satisfactoria
+                If response.StatusCode <> HttpStatusCode.OK Then
+                    MessageBox.Show("Server Error( " + response.StatusCode + ": " + response.StatusDescription + ").")
+                    Return -1
+                Else
+                    'como se obtiene datos en Stream se pasan a un StreamReader para facilitar su lectura
+                    Dim reader As StreamReader = New StreamReader(response.GetResponseStream())
+                    'Se lee hasta el final y se le hace un parse para delimitar el rango de busqueda en el JSON
+                    Dim parsejson = JToken.Parse(reader.ReadToEnd())("bmx")("series")
+                    'Obteniendo el valor indicado
+                    For Each item As JToken In parsejson
+                        For Each i2 As JToken In item("datos")
+                            item = i2("dato")
+                            response.Close()
+                            Return item
+                        Next
+                    Next
+                End If
+            End Using
+        Catch ex As Exception
+            Return -1
+        End Try
+        Return -1
+    End Function
+
+End Class
